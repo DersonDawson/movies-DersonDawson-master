d7a85d34f896476a2de06b3d02e0f996e9999300
DersonDawson <andersonsmurf@gmail.com>
2022-11-13 10:08:08 -0300

    Initial commit

---
+++ src/header/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Netflix clone, made with react</p>
+      
+    </footer>
+  );
+};
+
+export default Footer;
